a8fecb0922af20f0bc6f00f27c4628225889f4fc
arsene_lupin <arsene_lupin@trunghieubh0102@gmail.com>
2026-09-10 21:50:33 +0700

    change path

--- lib/test/parameter/ostrack.py
+++ lib/test/parameter/ostrack.py
@@ -21,8 +21,7 @@ def parameters(yaml_name: str):
     params.search_size = cfg.TEST.SEARCH_SIZE
 
     # Network checkpoint path
-    params.checkpoint = os.path.join(save_dir, "checkpoints/train/ostrack/%s/OSTrack_ep%04d.pth.tar" %
-                                     (yaml_name, cfg.TEST.EPOCH))
+    params.checkpoint = "/kaggle/input/models/huynhat15/ostrack-ft1/pytorch/default/1/OSTrack_ep0005.pth.tar"
 
     # whether to save boxes from all queries
     params.save_all_boxes = False
